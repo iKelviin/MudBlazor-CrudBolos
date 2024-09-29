497aa409eaba8f50d5636cb584124788c9e9fc37
Kelvin Santos <kelvinsantosdarocha@gmail.com>
2024-09-28 23:32:38 -0300

    Initial Commit

---
+++ Data/Bolo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudBolos.Data
+{
+    public class Bolo
+    {
+        public int Id { get; set; }
+        [MaxLength(200)]
+        public string? Nome { get; set; }
+        [MaxLength(300)]
+        public string? Descricao { get; set; }
+        [MaxLength(200)]
+        public string? ImagemUrl { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Preco { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using MudBlazor.Services;
+using CrudBolos.Components;
+using CrudBolos.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration
+                                .GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
